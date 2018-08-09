476c15daa3abd37c0c5a862ff61c27f8aa38ddbe
Nick W <32316588+Nick-Winkler@users.noreply.github.com>
2018-08-08 21:37:34 -0500

    migrate to arrow functios and add new event removal method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,15 @@
+// sets shorter names to select specific elements in the page
 const button = document.getElementById('inputbutton');
 const field = document.getElementById('inputfield');
 const todoList = document.getElementById('todo-list');
 
-/*
-  As this gets more complex, I will add additional comments, but for now
-  the functions and code are self-commenting and should be easy to follow
-*/
-
-// I can't turn this function declaration into an arrow function, because
-// the arrow functions cant use the 'this' keyword
-function removeEl() {
-  this.parentNode.removeChild(this);
+// removes the current target of the event that's passed
+let removeEl = (event) => {
+  event.currentTarget.remove();
 }
 
-function addToList() {
+// adds the new input to the list as a list item
+const addToList = () => {
   const node = document.createElement('li');
   const textNode = document.createTextNode(field.value);
   node.appendChild(textNode);
@@ -22,19 +18,20 @@ function addToList() {
 }
 
 // checks to see if the input has a non-empty value in it before adding to the list
-function sendInput() {
+const sendInput = () => {
   if (field.value.length > 0) {
     addToList();
     field.value = ''; // clears the input field after adding the input to the list
   }
 }
 
-function checkKey(e) {
-  // keyCode 13 is the Enter key
+// checks that there's a value in the input and that the enter key was pressed
+const checkKey = (e) => {
   if (field.value.length > 0 && e.keyCode === 13) {
     sendInput();
   }
 }
 
+// adds event listeners to the button and field to watch for user input
 button.addEventListener('click', sendInput);
 field.addEventListener('keypress', checkKey);

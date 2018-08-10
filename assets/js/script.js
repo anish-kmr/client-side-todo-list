6fb3f69db7f0b64d36ddd69c5a0478f7d43013ae
Nick W <32316588+Nick-Winkler@users.noreply.github.com>
2018-08-09 20:23:12 -0500

    replace current delete task feature with delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,48 @@
+// Todo: add unit testing so that when I add more features, I don't have to test everything each time
+
 // sets shorter names to select specific elements in the page
-const button = document.getElementById('inputbutton');
-const field = document.getElementById('inputfield');
+const inputButton = document.getElementById('inputbutton');
+const inputField = document.getElementById('inputfield');
 const todoList = document.getElementById('todo-list');
 
-// removes the current target of the event that's passed
-let removeEl = (event) => {
-  event.currentTarget.remove();
+// removes the parent node of the current event
+const removeEl = (event) => {
+  event.currentTarget.parentNode.remove();
+}
+
+// adds a delete button that, when clicked, deletes the li element and the new button
+const addDelButton = (li) => {
+  const button = document.createElement('button');
+  const buttonText = document.createTextNode('delete');
+  button.appendChild(buttonText);
+  li.appendChild(button);
+  button.addEventListener('click', removeEl);
 }
 
 // adds the new input to the list as a list item
 const addToList = () => {
-  const node = document.createElement('li');
-  const textNode = document.createTextNode(field.value);
-  node.appendChild(textNode);
-  todoList.appendChild(node);
-  node.addEventListener('click', removeEl);
+  const li = document.createElement('li');
+  const liText = document.createTextNode(inputField.value);
+  li.appendChild(liText);
+  addDelButton(li); // adds a delete button alongside the new li element
+  todoList.appendChild(li);
 }
 
 // checks to see if the input has a non-empty value in it before adding to the list
 const sendInput = () => {
-  if (field.value.length > 0) {
+  if (inputField.value.length > 0) {
     addToList();
-    field.value = ''; // clears the input field after adding the input to the list
+    inputField.value = ''; // clears the input field after adding the input to the list
   }
 }
 
 // checks that there's a value in the input and that the enter key was pressed
 const checkKey = (e) => {
-  if (field.value.length > 0 && e.keyCode === 13) {
+  if (inputField.value.length > 0 && e.keyCode === 13) {
     sendInput();
   }
 }
 
 // adds event listeners to the button and field to watch for user input
-button.addEventListener('click', sendInput);
-field.addEventListener('keypress', checkKey);
+inputButton.addEventListener('click', sendInput);
+inputField.addEventListener('keypress', checkKey);
